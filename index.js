e88f743f0246be4a61ce358a7802eeccde07b410
Michael Swain <michaelswain@tommy.com>
2025-07-12 09:55:43 +0200

    add hello world function and unit test

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("hello from index.js");
+export const helloWorld = () => {
+    return 'hello world';
+}
---
+++ tests/intro.test.js
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import { helloWorld } from "..";
+
+describe("hello world", () => {
+    it("should return the string 'hello world'", () => {
+        const expectedValue = 'hello world';
+        
+        const result = helloWorld();
+
+        expect(result).toBe(expectedValue);
+    })
+})
